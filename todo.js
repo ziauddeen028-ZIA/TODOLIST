db1fd9c4121cf815dc5f31b65bb1ec9e2f7aae0e
ziauddeen028-ZIA <ziauddeen028@gmail.com>
2025-12-10 19:48:37 +0530

    Code Updated

--- todo.js
+++ todo.js
@@ -1,24 +1,71 @@
- 
-        let inputValue = document.getElementById("list-input");
-        let listitem = document.getElementById("list-container");
-        
-
-        //add elements
-        function adding() {
-            let listElement = document.createElement("li");
-            listElement.innerHTML = inputValue.value +"<button id=finish onclick=finishing(event)>finish</button><button id=del onclick=deleting(event)>delete</button>";
-            listitem.append(listElement);
-        }
-
-        //deleting elements
-        function deleting(event) {
-            event.target.parentElement.remove();
-        }
-
-        function finishing(event) {
-             event.target.parentElement.style.backgroundColor="lightgreen";
-             event.target.parentElement.style.color="black";
-             event.target.remove();
-        }
+let taskInput = document.getElementById("list-input");
+let addbutton = document.getElementById("add");
+let listItem = document.getElementById("list-container");
 
 
+addbutton.addEventListener("click", addTask);
+
+loadTasks();
+
+function addTask() {
+    let task = taskInput.value.trim();
+
+    if (task) {
+        createTaskElement(task);
+        taskInput.value = " ";
+        saveTasks();
+    }
+    else {
+        alert("Please Enter Task...!")
+    }
+}
+
+function createTaskElement(task) {
+    let listElement = document.createElement("li");
+    // listElement.textContent = task;
+    let taskText = document.createElement("span");
+    taskText.textContent=task;
+
+    let buttonDiv = document.createElement("div");
+    buttonDiv.classList.add("buttons");
+
+    let deletebtn = document.createElement("button");
+    deletebtn.textContent = "delete";
+    deletebtn.classList.add("delete");
+
+    let finishbtn = document.createElement("button");
+    finishbtn.textContent = "finish";
+    finishbtn.classList.add("finish");
+
+    deletebtn.addEventListener("click", () => {
+        listItem.removeChild(listElement);
+        saveTasks();
+    });
+
+    finishbtn.addEventListener("click", () => {
+        listElement.style.backgroundColor = "lightgreen";
+        finishbtn.style.display = "none";
+    });
+
+    buttonDiv.appendChild(deletebtn);
+    buttonDiv.appendChild(finishbtn);
+
+    listElement.appendChild(taskText);
+    listElement.appendChild(buttonDiv);
+
+    listItem.appendChild(listElement);
+}
+
+function saveTasks() {
+    let tasks = [];
+    listItem.querySelectorAll("li").forEach(function (item) {
+        tasks.push(item.textContent.replace('delete', '').replace('finish', '').trim());
+    });
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function loadTasks() {
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.forEach(createTaskElement);
+}
